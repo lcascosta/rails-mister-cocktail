de26a887a358b4673925cc3913619dd1e45b0f5c
Lucas Guimaraes <lcascosta@hotmail.com>
2017-04-13 12:45:49 -0300

    all controller and routes structure implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+10.times do
+  ingr_hash = user["drinks"].sample
+  ingr_name = ingr_hash["strIngredient1"]
+  Ingredient.create(name: ingr_name)
+end
+
